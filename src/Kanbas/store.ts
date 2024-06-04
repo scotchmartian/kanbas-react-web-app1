e3aeaa690bf7c5d4a93a51e70f20f6bcee887213
Scott Martin <defaburneracct@gmail.com>
2024-06-04 12:39:23 -0400

    a4 Redux

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
-import assignmentsred
 const store = configureStore({
   reducer: {
     modulesReducer,
